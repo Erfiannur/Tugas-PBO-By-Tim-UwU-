05f2a22a5431cbc5c135ec62304c1029e6fc7731
Erfiannur <76195424+Erfiannur@users.noreply.github.com>
2020-12-19 14:55:08 +0700

    Add files via upload

---
+++ method.php
@@ -0,0 +1,22 @@
+<?php
+//Cara penulisan class dan property OOP PHP - @tim_UwU
+
+class buah{
+
+    // property oop
+    var $warna;
+    var $berat;
+    var $rasa;
+ 
+   //method oop
+   function dimakan (){
+       //isi method
+}
+   function bermanfaat (){
+           //isi mehod
+   }
+
+
+}
+
+?>
