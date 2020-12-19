c24af0acb5839305054e43b01492b64b342a32ee
Erfiannur <76195424+Erfiannur@users.noreply.github.com>
2020-12-19 14:29:26 +0700

    Add files via upload

---
+++ property.php
@@ -0,0 +1,15 @@
+<?php>
+//Cara penulisan class dan property OOP PHP - @tim_UwU
+
+class buah{             //class buah
+
+    var $warna;         //property warna
+
+    var $berat;           //property berat
+
+    var $rasa;            //property rasa
+
+
+}
+?>
+
